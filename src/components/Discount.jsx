3b10e7b7dd9d8664395051aa619891bb3461c223
Pradipta R. Sandri <pradiptaramadan9c@gmail.com>
2025-06-29 10:13:51 +0700

    feat(fee): add other fee optional value (#1)

--- src/components/Discount.jsx
+++ src/components/Discount.jsx
@@ -1,12 +1,23 @@
+// "use client"
 import handleKeyDown from "@/utils/handleKey";
 import { Card, CardContent } from "./ui/card";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
+import { useState } from "react";
+import { Button } from "./ui/button";
+import { FaRegTrashAlt } from "react-icons/fa";
 
 const Discount = ({ total, setTotal }) => {
+  const [showOtherFee, setShowOtherFee] = useState(false);
+
   const handleTotalChange = (e) => {
     const { name, value } = e.target;
     setTotal({ ...total, [name]: value === "" ? 0 : parseFloat(value) });
+  };
+
+  const handleRemoveOtherFee = () => {
+    setTotal({ ...total, otherFee: 0 });
+    setShowOtherFee(false);
   };
 
   return (
@@ -52,6 +63,43 @@ const Discount = ({ total, setTotal }) => {
               className="no-arrows"
             />
           </div>
+        </div>
+
+
+        <div className="flex flex-col pt-4 w-full gap-2">
+          {!showOtherFee ? (
+            <Button
+              variant="neutral"
+              type="button"
+              onClick={() => setShowOtherFee(true)}
+            >
+              Tambah Biaya Lainnya
+            </Button>
+          ) : (
+            <div className="flex  gap-2 flex-row items-end md:items-center">
+              <div className="flex gap-2 flex-col w-full">
+                <Label>Biaya Lainnya</Label>
+                <Input
+                  type="number"
+                  name="otherFee"
+                  value={total.otherFee === 0 ? "" : total.otherFee}
+                  onChange={handleTotalChange}
+                  placeholder="Biaya lainnya"
+                  onKeyDown={handleKeyDown}
+                  min="0"
+                  className="no-arrows"
+                />
+              </div>
+              <Button
+                className="md:self-end bg-bw"
+                onClick={handleRemoveOtherFee}
+                variant="reverse"
+              >
+                <FaRegTrashAlt />
+              </Button>
+
+            </div>
+          )}
         </div>
       </CardContent>
     </Card>
